b6c8e13b9efe858d3bc63a4b0422fae0106de8d8
Gawsan <67488752+Gawsan@users.noreply.github.com>
2022-03-05 00:32:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ssdcheck/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.ssdcheck;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity<EXTRA_MESSAGE> extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE="Activity one tute one";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, displayMessage.class);
+        EditText editText;
+        editText = findViewById(R.id.message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message); startActivity(intent);
+
+    }
+}
---
+++ app/src/main/java/com/example/ssdcheck/displayMessage.java
@@ -0,0 +1,21 @@
+package com.example.ssdcheck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class displayMessage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.displayMessage);
+        textView.setText(message);
+    }
+}
